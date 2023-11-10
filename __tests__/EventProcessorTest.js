19843d1b57b1e41dbf041721071125311eeae7c1
JunSeok <ppu1234@naver.com>
2023-11-10 15:35:46 +0900

    test: 혜택 내역 저장 테스트 코드 작성

--- __tests__/EventProcessorTest.js
+++ __tests__/EventProcessorTest.js
@@ -5,7 +5,7 @@ describe('EventProcessor 클래스 테스트', () => {
   test('할인 전 총주문 금액 계산', () => {
     // Arrange
     const visitDate = 5;
-    const orderedMenu = new Map([
+    const selectedMenu = new Map([
       ['티본스테이크', 1],
       ['비비큐립', 1],
       ['초코케이크', 2],
@@ -14,7 +14,7 @@ describe('EventProcessor 클래스 테스트', () => {
     const expectedTotalAmountBeforeDiscount = 142_000;
 
     // Act
-    const processor = new EventProcessor(visitDate, orderedMenu);
+    const processor = new EventProcessor(visitDate, selectedMenu);
     const totalAmountBeforeDiscount = processor.getTotalAmountBeforeDiscount();
 
     // Assert
@@ -22,31 +22,68 @@ describe('EventProcessor 클래스 테스트', () => {
   });
 
   test.each([
-    [
-      new Map([
+    {
+      selectedMenu: new Map([
         ['티본스테이크', 1],
         ['비비큐립', 1],
         ['초코케이크', 2],
         ['제로콜라', 1],
       ]),
-      BENEFIT_CONSTANT.gift,
-    ],
-    [
-      new Map([
+      expectedGift: BENEFIT_CONSTANT.gift,
+    },
+    {
+      selectedMenu: new Map([
         ['타파스', 1],
         ['제로콜라', 1],
       ]),
-      BENEFIT_CONSTANT.nothing,
-    ],
-  ])('증정 메뉴 확인', (orderedMenu, expectedGift) => {
+      expectedGift: BENEFIT_CONSTANT.nothing,
+    },
+  ])('증정 메뉴 확인', (test) => {
     // Arrange
     const visitDate = 5;
+    const { selectedMenu, expectedGift } = test;
 
     // Act
-    const processor = new EventProcessor(visitDate, orderedMenu);
+    const processor = new EventProcessor(visitDate, selectedMenu);
     const checkGiftResult = processor.getCheckGiftResult();
 
     // Assert
     expect(checkGiftResult).toBe(expectedGift);
+  });
+
+  test.each([
+    {
+      selectedMenu: new Map([
+        ['티본스테이크', 1],
+        ['비비큐립', 1],
+        ['초코케이크', 2],
+        ['제로콜라', 1],
+      ]),
+      visitDate: 3,
+      expectedBenefitsResult: new Map([
+        [BENEFIT_CONSTANT.benefitNames.dDayDiscount, 1200],
+        [BENEFIT_CONSTANT.benefitNames.weekDay, 4046],
+        [BENEFIT_CONSTANT.benefitNames.specialDiscount, 1000],
+        [BENEFIT_CONSTANT.benefitNames.giftEvent, 25000],
+      ]),
+    },
+    {
+      selectedMenu: new Map([
+        ['타파스', 1],
+        ['제로콜라', 1],
+      ]),
+      visitDate: 26,
+      expectedBenefitsResult: BENEFIT_CONSTANT.nothing,
+    },
+  ])('혜택 내역 저장', (test) => {
+    // Arrange
+    const { selectedMenu, visitDate, expectedBenefitsResult } = test;
+
+    // Act
+    const processor = new EventProcessor(visitDate, selectedMenu);
+    const benefitsResult = processor.getBenefitsResult();
+
+    // Assert
+    expect(benefitsResult).toEqual(expectedBenefitsResult);
   });
 });

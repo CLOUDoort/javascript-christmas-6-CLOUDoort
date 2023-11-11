8a85779b24e9f18d0757e0214a3d869fd0335093
JunSeok <ppu1234@naver.com>
2023-11-12 00:42:17 +0900

    test: 메뉴 입력 예외 처리 테스트 코드 작성

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -12,13 +12,14 @@ describe('Validator 클래스 테스트.', () => {
     {
       visitDate: 'test',
     },
-  ])('예상 방문 날짜 예외 처리', ({ visitDate }) => {
+  ])('예상 방문 날짜 예외 처리.', ({ visitDate }) => {
     // Act
     const resultFn = () => Validator.validateVisitDate(visitDate);
 
     // Assert
     expect(resultFn).toThrow(ErrorMessages.visitDate);
   });
+
   test.each([
     {
       visitDate: 1,
@@ -26,11 +27,53 @@ describe('Validator 클래스 테스트.', () => {
     {
       visitDate: 15,
     },
-  ])('예상 방문 날짜 정상 작동', ({ visitDate }) => {
+  ])('예상 방문 날짜 정상 동작.', ({ visitDate }) => {
     // Act
     const resultFn = () => Validator.validateVisitDate(visitDate);
 
     // Assert
     expect(resultFn).not.toThrow(ErrorMessages.visitDate);
+  });
+
+  test.each([
+    {
+      selectedMenus: '해산물파스타-2,레드와인-1,초코케이크-1',
+    },
+    {
+      selectedMenus: '타파스-1,제로콜라-1',
+    },
+  ])('메뉴 입력 예외 처리.', ({ selectedMenus }) => {
+    // Act
+    const resultFn = () => Validator.validateSelectedMenus(selectedMenus);
+
+    // Assert
+    expect(resultFn).not.toThrow(ErrorMessages.menu);
+  });
+
+  test.each([
+    {
+      selectedMenus: '해산물파스타-2-3레드와인-1-2',
+    },
+    {
+      selectedMenus: '타파스-0,제로콜라-1',
+    },
+    {
+      selectedMenus: '해산물파스타-2,해산물파스타-1',
+    },
+    {
+      selectedMenus: '후라이드치킨-1',
+    },
+    {
+      selectedMenus: '제로콜라-1',
+    },
+    {
+      selectedMenus: '해산물파스타-21',
+    },
+  ])('메뉴 입력 정상 동작.', ({ selectedMenus }) => {
+    // Act
+    const resultFn = () => Validator.validateSelectedMenus(selectedMenus);
+
+    // Assert
+    expect(resultFn).toThrow(ErrorMessages.menu);
   });
 });

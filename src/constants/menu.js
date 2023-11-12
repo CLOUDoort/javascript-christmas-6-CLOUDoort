7f525faa442d6f84f1ef1324787a9ab6769fafa4
JunSeok <ppu1234@naver.com>
2023-11-12 14:38:30 +0900

    feat: 상수 메뉴 객체

--- src/constants/menu.js
+++ src/constants/menu.js
@@ -3,7 +3,7 @@ const MENU_PRICE = Object.freeze({
   타파스: 5_500,
   시저샐러드: 8_000,
   티본스테이크: 55_000,
-  비비큐립: 54_000,
+  바비큐립: 54_000,
   해산물파스타: 35_000,
   크리스마스파스타: 25_000,
   초코케이크: 15_000,
@@ -15,9 +15,15 @@ const MENU_PRICE = Object.freeze({
 
 const MENU_CATEGORY = Object.freeze({
   appetizer: ['양송이수프', '타파스', '시저샐러드'],
-  main: ['티본스테이크', '비비큐립', '해산물파스타', '크리스마스파스타'],
+  main: ['티본스테이크', '바비큐립', '해산물파스타', '크리스마스파스타'],
   dessert: ['초코케이크', '아이스크림'],
   beverage: ['제로콜라', '레드와인', '샴페인'],
 });
 
-export { MENU_PRICE, MENU_CATEGORY };
+const MENU_VALIDATE = Object.freeze({
+  menuMin: 1,
+  menuMax: 20,
+  menuTypeLength: 2,
+});
+
+export { MENU_PRICE, MENU_CATEGORY, MENU_VALIDATE };

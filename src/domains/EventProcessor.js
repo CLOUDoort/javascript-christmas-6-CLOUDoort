613aac8175f2cfc4524abaeb435b2ec9133f83f4
JunSeok <ppu1234@naver.com>
2023-11-13 14:33:47 +0900

    refactor: eventProcessor 메서드 함수 분리

--- src/domains/EventProcessor.js
+++ src/domains/EventProcessor.js
@@ -1,6 +1,9 @@
-import BENEFIT_CONSTANT from '../constants/benefit.js';
 import { MENU_PRICE } from '../constants/menu.js';
-import storeBenefits from '../utils/storeBenefits.js';
+import checkBenefitsResult from '../utils/benefits/checkBenefitsResult.js';
+import checkEventBadge from '../utils/benefits/checkEventBadge.js';
+import checkGiftResult from '../utils/benefits/checkGift.js';
+import checkTotalAmountAfterDiscount from '../utils/benefits/checkTotalAmountAfterDiscount.js';
+import checkTotalBenefitsAmount from '../utils/benefits/checkTotalBenefitsAmount.js';
 
 class EventProcessor {
   #visitDate;
@@ -13,9 +16,7 @@ class EventProcessor {
   }
 
   getTotalAmountBeforeDiscount() {
-    const selectedMenus = this.#selectedMenus;
-
-    return Array.from(selectedMenus).reduce(
+    return Array.from(this.#selectedMenus).reduce(
       (total, [selectedMenu, count]) =>
         total + MENU_PRICE[selectedMenu] * count,
       0,
@@ -24,41 +25,26 @@ class EventProcessor {
 
   getCheckGiftResult() {
     const totalAmountBeforeDiscount = this.getTotalAmountBeforeDiscount();
-    if (totalAmountBeforeDiscount >= BENEFIT_CONSTANT.gift.amountLimit) {
-      return BENEFIT_CONSTANT.gift.item;
-    }
 
-    return BENEFIT_CONSTANT.nothing;
+    return checkGiftResult(totalAmountBeforeDiscount);
   }
 
   getBenefitsResult() {
-    const totalAmountBeforeDiscount = this.getTotalAmountBeforeDiscount();
-    if (totalAmountBeforeDiscount < BENEFIT_CONSTANT.limit) {
-      return BENEFIT_CONSTANT.nothing;
-    }
-
-    const visitDate = this.#visitDate;
-    const selectedMenu = this.#selectedMenus;
-    const benefits = new Map([]);
     const giftResult = this.getCheckGiftResult();
-    storeBenefits(benefits, visitDate, giftResult, selectedMenu);
+    const totalAmountBeforeDiscount = this.getTotalAmountBeforeDiscount();
 
-    if (benefits.size === 0) return BENEFIT_CONSTANT.nothing;
-    return benefits;
+    return checkBenefitsResult(
+      this.#visitDate,
+      this.#selectedMenus,
+      giftResult,
+      totalAmountBeforeDiscount,
+    );
   }
 
   getTotalBenefitsAmount() {
     const totalBenefits = this.getBenefitsResult();
-    if (totalBenefits === BENEFIT_CONSTANT.nothing) {
-      return BENEFIT_CONSTANT.nothingAmount;
-    }
 
-    const totalBenefitsAmount = Array.from(totalBenefits).reduce(
-      (acc, [_, amount]) => acc + amount,
-      0,
-    );
-
-    return totalBenefitsAmount;
+    return checkTotalBenefitsAmount(totalBenefits);
   }
 
   getTotalAmountAfterDiscount() {
@@ -67,29 +53,17 @@ class EventProcessor {
     const totalAmountAfterDiscount =
       totalAmountBeforeDiscount - totalBenefitsAmount;
 
-    if (totalBenefitsAmount === BENEFIT_CONSTANT.nothingAmount) {
-      return totalAmountBeforeDiscount;
-    }
-    if (totalAmountBeforeDiscount >= BENEFIT_CONSTANT.gift.amountLimit) {
-      return totalAmountAfterDiscount + BENEFIT_CONSTANT.gift.amount;
-    }
-    return totalAmountAfterDiscount;
+    return checkTotalAmountAfterDiscount(
+      totalBenefitsAmount,
+      totalAmountBeforeDiscount,
+      totalAmountAfterDiscount,
+    );
   }
 
   getEventBadge() {
     const totalBenefitsAmount = this.getTotalBenefitsAmount();
-    if (totalBenefitsAmount === BENEFIT_CONSTANT.nothingAmount) {
-      return BENEFIT_CONSTANT.nothing;
-    }
 
-    if (totalBenefitsAmount < BENEFIT_CONSTANT.eventBadge.tree.limit) {
-      return BENEFIT_CONSTANT.eventBadge.star.item;
-    }
-
-    if (totalBenefitsAmount < BENEFIT_CONSTANT.eventBadge.santa.limit) {
-      return BENEFIT_CONSTANT.eventBadge.tree.item;
-    }
-    return BENEFIT_CONSTANT.eventBadge.santa.item;
+    return checkEventBadge(totalBenefitsAmount);
   }
 }
 
---
+++ src/utils/benefits/checkTotalBenefitsAmount.js
@@ -0,0 +1,14 @@
+import BENEFIT_CONSTANT from '../../constants/benefit.js';
+
+const checkTotalBenefitsAmount = (totalBenefits) => {
+  if (totalBenefits === BENEFIT_CONSTANT.nothing) {
+    return BENEFIT_CONSTANT.nothingAmount;
+  }
+
+  return Array.from(totalBenefits).reduce(
+    (acc, [_, amount]) => acc + amount,
+    0,
+  );
+};
+
+export default checkTotalBenefitsAmount;
